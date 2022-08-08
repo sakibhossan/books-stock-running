edc05b0a74a9417ea2cd494a5545e19c5272fc84
sakib213 <sakibhss438@gmail.com>
2022-08-08 14:03:50 +0600

    create google login

--- src/Pages/Login/LoginSocial/LoginSocial.js
+++ src/Pages/Login/LoginSocial/LoginSocial.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import google from '../../../Images/google.png';
 
 const LoginSocial = () => {
     return (
@@ -7,6 +8,13 @@ const LoginSocial = () => {
             <div style={{height:'1px'}} className='bg-dark w-50'></div>
                 <p className='mt-2 px-2'>or</p>
                 <div style={{height:'1px'}} className='bg-dark w-50'></div>
+            </div>
+            <div>
+                <button className='btn btn-light rounded-pill border border-dark mx-auto w-50 d-block'>
+                    <img src={google} alt="" />
+                    <span className='px-2'>Google Sign In</span>
+                    
+                </button>
             </div>
         </div>
     );
