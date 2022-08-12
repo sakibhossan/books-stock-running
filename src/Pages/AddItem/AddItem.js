cc433c28ab7cb11af5a9608b02c97e5ce4744c3e
sakib213 <sakibhss438@gmail.com>
2022-08-12 09:34:56 +0600

    react form hooks set up

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -3,20 +3,34 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+
+        console.log(data);
+        const url =`http://localhost:5000/product`;
+        fetch(url,{
+            method: 'POST',
+        headers: {
+            'content-type': 'application/json'   
+        },
+        body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => {
+            console.log(result)
+        })
+    };
+
     return (
         <div className='w-50 mx-auto'>
-            <h2>Please add a  product</h2>
-            <form
-            className='d-flex flex-column'
-             onSubmit={handleSubmit(onSubmit)}>
-      <input className='mb-2' placeholder="Name" {...register("name")} />
-      <textarea className='mb-2' placeholder="description" {...register("description")} />
-      <input className='mb-2' placeholder="Price" type="number" {...register("price")} />
-      <input className='mb-2' placeholder="quantity" type="number" {...register("quantity")} />
-      <input className='mb-2' placeholder="Supplier Name" {...register("supplier name")} />
-      <input className='mb-2' placeholder="Photo URL" {...register("img")} />
-      <input className='mx-auto w-50 ' type="submit" value="Add Item" />
+            <h2>this is add service</h2>
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+      <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+      <textarea className='mb-2' placeholder='Description' {...register("description")} />
+      <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+      <input className='mb-2' placeholder='Quantity' type="number" {...register("quantity")} />
+      <input className='mb-2' placeholder='PhotoURL' type="text" {...register("img")} />
+      <input className='mb-2' placeholder='Supplier Name' type="text" {...register("supplier")} />
+      <input type="submit" value="Add Service" />
     </form>
         </div>
     );
